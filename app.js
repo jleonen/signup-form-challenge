97e60cad46f5a7b769f8b38cb111ab0e37e5da0b
jleonen <jleonen2012@gmail.com>
2021-10-15 23:53:59 -0700

    Refactored and added new function to display invalid email message.

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ const firstName = document.querySelector(".firstName");
 const lastName = document.querySelector(".lastName");
 const email = document.querySelector(".email");
 const password = document.querySelector(".password");
-
-// const firstNameErr = document.querySelector(".first-error");
-// const lastNameErr = document.querySelector(".last-error");
-// const emailErr = document.querySelector(".email-error");
-// const passwordErr = document.querySelector(".password-error");
-
+const emailValid = document.querySelector(".error2-2");
 const formArray = [firstName, lastName, email, password];
-// const errArray = [firstNameErr, lastNameErr, emailErr, passwordErr];
 
 let firstNameErr,
   lastNameErr,
@@ -18,87 +12,102 @@ let firstNameErr,
   invalidEmail,
   passwordErr = false;
 
-const checkEmail = function (entry, i) {
-  if (
-    entry.classList.contains("email") &&
-    entry.value.includes("@") &&
-    // invalidEmail &&
-    // (entry.classList.contains("email") &&
-    entry.value.includes(".com") //&&
-    // !invalidEmail)
-  ) {
-    ///////
-    if (entry.nextElementSibling) {
-      document.querySelector(`.error${i}-2`).style.display = "none";
-      invalidEmail = false;
+emailValid.style.display = "none";
+// const invalidEmailMsg = function (entry, i) {
+//   let html = `<blockquote class="error${i}-2">Looks like this is not an email.</blockquote>`;
+//   entry.insertAdjacentHTML("afterend", html);
+//   entry.style.backgroundImage = "url('images/icon-error.svg')";
+//   entry.style.backgroundRepeat = "no-repeat";
+//   entry.style.backgroundPosition = "right";
+// };
+
+const invalidEmailMsg = function () {
+  // let html = `<blockquote class="error2-2">Looks like this is not an email.</blockquote>`;
+  // email.insertAdjacentHTML("afterend", html);
+  email.style.backgroundImage = "url('images/icon-error.svg')";
+  email.style.backgroundRepeat = "no-repeat";
+  email.style.backgroundPosition = "right";
+  emailValid.style.display = "block";
+};
+
+// const checkEmail = function (entry, i) {
+//   if (
+//     entry.classList.contains("email") &&
+//     entry.value.includes("@") &&
+//     entry.value.includes(".com")
+//   ) {
+//     invalidEmail = false;
+//     !invalidEmail
+//       ? console.log("Valid email")
+//       : (document.querySelector(`.error${i}-2`).style.display = "none");
+//   } else if (
+//     (entry.value.includes(".com") && !invalidEmail) ||
+//     (entry.value.includes("@") && !invalidEmail)
+//   ) {
+//     invalidEmailMsg(entry, i);
+//     invalidEmail = true;
+//   } else if (
+//     (entry.classList.contains("email") &&
+//       !entry.value.includes(".com") &&
+//       !invalidEmail) ||
+//     (!entry.value.includes("@") && !invalidEmail)
+//   ) {
+//     invalidEmailMsg(entry, i);
+//     invalidEmail = true;
+//   } else {
+//     console.log("Invalid email");
+//   }
+// };
+
+const checkEmail = function () {
+  if (email.value.includes("@") && email.value.includes(".com")) {
+    if (!invalidEmail) {
+      console.log("Valid email");
     } else {
+      emailValid.style.display = "none";
       invalidEmail = false;
     }
-
-    ///////
+  } else if (email.value === "") {
+    document.querySelector(`.error2-2`).style.display = "none";
+    // let html = `<blockquote class="error2">Email cannot be empty.</blockquote>`;
+    // email.insertAdjacentHTML("afterend", html);
+    // emailErr = true;
   } else if (
-    (entry.value.includes(".com") && !invalidEmail) ||
-    (entry.value.includes("@") && !invalidEmail)
+    (email.value.includes(".com") && !invalidEmail) ||
+    (email.value.includes("@") && !invalidEmail)
   ) {
-    // if (entry.nextElementSibling) {
-    let html = `<blockquote class="error${i}-2">Looks like an invalid email.</blockquote>`;
-    entry.insertAdjacentHTML("afterend", html);
-    entry.style.backgroundImage = "url('images/icon-error.svg')";
-    entry.style.backgroundRepeat = "no-repeat";
-    entry.style.backgroundPosition = "right";
+    invalidEmailMsg();
     invalidEmail = true;
-    // document.querySelector(`.error${i}-2`).style.display = "none";
-
-    // entry.style.backgroundImage = "none";
   } else if (
-    (!entry.value.includes(".com") && !invalidEmail) ||
-    (!entry.value.includes("@") && !invalidEmail)
+    (!email.value.includes(".com") && !invalidEmail) ||
+    (!email.value.includes("@") && !invalidEmail)
   ) {
-    let html = `<blockquote class="error${i}-2">Looks like an invalid email.</blockquote>`;
-    entry.insertAdjacentHTML("afterend", html);
-    entry.style.backgroundImage = "url('images/icon-error.svg')";
-    entry.style.backgroundRepeat = "no-repeat";
-    entry.style.backgroundPosition = "right";
+    invalidEmailMsg();
     invalidEmail = true;
   } else {
-    console.log("no sibling element");
+    console.log("Invalid email");
   }
 };
 
 const renderError = function (data, i) {
   if (
     data.classList.contains("firstName") &&
-    !firstNameErr
-    // !data.classList.contains("-error")
+    data.classList.contains("-error")
   ) {
-    // data.classList.toggle("-error");
     let html = `<blockquote class="error${i}">First name cannot be empty</blockquote>`;
     data.insertAdjacentHTML("afterend", html);
     firstNameErr = true;
-  } else if (
-    data.classList.contains("lastName") &&
-    !lastNameErr
-    // !data.classList.contains("-error")
-  ) {
-    // data.classList.toggle("-error");
+  } else if (data.classList.contains("lastName") && !lastNameErr) {
     let html = `<blockquote class="error${i}">Last name cannot be empty</blockquote>`;
     data.insertAdjacentHTML("afterend", html);
     lastNameErr = true;
-  } else if (
-    data.classList.contains("email") &&
-    !emailErr
-    // !data.classList.contains("-error")
-  ) {
-    // data.classList.toggle("-error");
+  } else if (data.classList.contains("email") && !emailErr) {
     let html = `<blockquote class="error${i}">Email cannot be empty.</blockquote>`;
     data.insertAdjacentHTML("afterend", html);
     emailErr = true;
-  } else if (
-    data.classList.contains("password") &&
-    !passwordErr
-    // !data.classList.contains("-error")
-  ) {
-    // data.classList.toggle("-error");
+    invalidEmail = false;
+    document.querySelector(`.error2-2`).style.display = "none";
+  } else if (data.classList.contains("password") && !passwordErr) {
     let html = `<blockquote class="error${i}">Password cannot be empty</blockquote>`;
     data.insertAdjacentHTML("afterend", html);
     passwordErr = true;
@@ -106,6 +115,21 @@ const renderError = function (data, i) {
     console.log("Field is still blank");
   }
 };
+
+// const renderError = function (data) {
+//     if(data.)
+//     let html1 = `<blockquote class="error0">First name cannot be empty</blockquote>`;
+//     data.insertAdjacentHTML("afterend", html1);
+
+//     let html2 = `<blockquote class="error1">Last name cannot be empty</blockquote>`;
+//     data.insertAdjacentHTML("afterend", html2);
+
+//     let html3 = `<blockquote class="error2">Email cannot be empty.</blockquote>`;
+//     data.insertAdjacentHTML("afterend", html3);
+
+//     let html4 = `<blockquote class="error3">Password cannot be empty</blockquote>`;
+//     data.insertAdjacentHTML("afterend", html4);
+// };
 
 const valueCheck = function (data, i) {
   if (!data.classList.contains("-error")) {
@@ -133,41 +157,14 @@ const submitValues = function () {
         document.querySelector(`.error${i}`).style.display = "none";
         console.log(`Error class removed for ${entry.classList.value}.`);
       }
-      // !invalidEmail ? checkEmail(entry, i) : console.log("Valid email");
+
       checkEmail(entry, i);
 
       if (entry.classList.value === "firstName") firstNameErr = false;
       if (entry.classList.value === "lastName") lastNameErr = false;
       if (entry.classList.value === "email") emailErr = false;
-      // if (
-      //   entry.classList.value === "email" &&
-      //   !entry.value.includes("@") &&
-      //   !invalidEmail
-      // ) {
-      //   let html = `<blockquote class="error${i}">Looks like this is not a valid email</blockquote>`;
-      //   entry.insertAdjacentHTML("afterend", html);
-      //   invalidEmail = true;
-      // } else if (
-      //   entry.classList.value === "email" &&
-      //   !entry.value &&
-      //   invalidEmail
-      // ) {
-      //   invalidEmail = false;
-      //   document.querySelector(`.error2-1`).style.display = "none";
-      // } else {
-      //   invalidEmail = false;
-      //   document.querySelector(`.error${i}-1`).style.display = "none";
-      // }
+
       if (entry.classList.value === "password") passwordErr = false;
     }
-    // if (entry.classList.value === "email" && !entry.value.includes("@")) {
-    //   let html = `<blockquote class="error${i}">Looks like this is not a valid email</blockquote>`;
-    //   entry.insertAdjacentHTML("afterend", html);
-    // } else {
-    //   emailErr
-    //     ? (document.querySelector(`.error${i}`).style.display = "none")
-    //     : console.log("no email error");
   });
 };
-//   });
-// };
